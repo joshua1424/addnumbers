6f8a36d24ded13b42f02ef9bc814d83f1d3ea184
John Varghese <lookitup4me@gmail.com>
2015-02-27 18:27:27 -0800

    Adds two numbers

---
+++ add.rb
@@ -0,0 +1,11 @@
+puts "Adding Numbers"
+
+puts "enter the first number"
+first_number = gets
+
+puts "enter the second number"
+second_number = gets
+
+sum = Integer(first_number) + Integer(second_number)
+puts "The sum is #{sum}" 
+
